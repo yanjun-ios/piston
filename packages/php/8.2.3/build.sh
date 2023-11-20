c15533871c44cbd5358d15d094ad67bd5a387381
Yan <jnyanm@08f8bc6ad860.ant.amazon.com>
2023-11-20 15:52:59 +0800

    update php

--- packages/php/8.2.3/build.sh
+++ packages/php/8.2.3/build.sh
@@ -11,10 +11,14 @@ tar xzf php.tar.gz --strip-components=1 -C php
 cd php
 
 
-./configure --prefix "$PREFIX"
+./configure --prefix "$PREFIX" --with-openssl --enable-mbstring
 
 make -j$(nproc)
 make install -j$(nproc)
 
 cd ../../
 rm -rf build
+
+# install aws sdk through .zip
+curl -s -o aws.zip https://docs.aws.amazon.com/aws-sdk-php/v3/download/aws.zip
+unzip aws.zip
