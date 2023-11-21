a8785461d4c91e7d458dc62f7cefca10e92fce85
Yan <jnyanm@08f8bc6ad860.ant.amazon.com>
2023-11-21 16:47:18 +0800

    update

--- packages/node/18.15.0/build.sh
+++ packages/node/18.15.0/build.sh
@@ -4,7 +4,16 @@ tar xf node.tar.xz --strip-components=1
 rm node.tar.xz
 
 # install aws sdk
-
 source environment
 
+# for sdk v2, support all the services
 ./bin/npm install aws-sdk --prefix ./
+
+# for sdk v3, current support ec2 s3 lambda dynamodb iam cloudfront ebs
+./bin/npm install @aws-sdk/client-ec2 --prefix ./
+./bin/npm install @aws-sdk/client-s3 --prefix ./
+./bin/npm install @aws-sdk/client-lambda --prefix ./
+./bin/npm install @aws-sdk/client-dynamodb --prefix ./
+./bin/npm install @aws-sdk/client-iam --prefix ./
+./bin/npm install @aws-sdk/client-cloudfront --prefix ./
+./bin/npm install @aws-sdk/client-ebs --prefix ./
