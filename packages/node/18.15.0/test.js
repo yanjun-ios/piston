fedfabb4506c35e015034f10db04e37818b37eb4
Yan <jnyanm@08f8bc6ad860.ant.amazon.com>
2023-11-21 17:28:07 +0800

    update

--- packages/node/18.15.0/test.js
+++ packages/node/18.15.0/test.js
@@ -1,1 +1,17 @@
-console.log('OK');
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+
+const region = "us-west-2";
+const client = new DynamoDB({ region });
+client.listTables({}, (err, data) => {
+    if (err) console.log(err, err.stack);
+    else console.log(data);
+});
+
+//import AWS from "aws-sdk";
+// var AWS = require("aws-sdk");
+// const region = "us-west-2";
+// const client = new AWS.DynamoDB({ region });
+// client.listTables({}, (err, data) => {
+//     if (err) console.log(err, err.stack);
+//     else console.log(data);
+// });
